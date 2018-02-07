92afd25aaca0899769804a4ac186b7c0946ba5f5
Jing <ojyy73@gmail.com>
2018-02-07 22:58:39 +0800

    Update test

--- test/java/seedu/addressbook/commands/ViewCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewCommandTest.java
@@ -49,7 +49,7 @@ public class ViewCommandTest {
         ReadOnlyPerson stranger = new Person(new Name("me"),
                                              new Phone("123", true),
                                              new Email("some@hey.go", true),
-                                             new Address("nus", false),
+                                             new Address("123, nus, #10-10, 12345", false),
                                              new UniqueTagList(Collections.emptySet()));
         List<ReadOnlyPerson> listWithExtraPerson
                 = new ArrayList<ReadOnlyPerson>(listWithAllTypicalPersons);
